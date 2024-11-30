c024ed144dbb75ec5384a4602e0621edf44fa9ef
Grzegorz Oleksa <grzesieq92@gmail.com>
2024-11-30 15:36:56 +0100

    added removing tasks function

--- index.js
+++ index.js
@@ -10,6 +10,16 @@
     },
   ];
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
   const init = () => {
     render();
 
@@ -30,22 +40,25 @@
     addNewTask(newTaskContent);
   };
 
-  const addNewTask = (newTaskContent) => {
-    tasks.push({ content: newTaskContent });
-    render();
-  };
-
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-        <li ${task.done ? 'style="text-decoration: line-through"' : ""}>${
-        task.content
-      }</li>
+        <li ${task.done ? 'style="text-decoration: line-through"' : ""}>
+        <button class="js-remove">usuń</button>
+        ${task.content}</li>
         `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, taskIndex) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(taskIndex);
+      });
+    });
   };
 
   init();
