d727f094ac890b63944d63d9cf8ec16b297c0563
Grzegorz Oleksa <grzesieq92@gmail.com>
2024-11-30 15:27:06 +0100

    added function for adding new tasks

--- index.js
+++ index.js
@@ -9,7 +9,29 @@
       done: true,
     },
   ];
+
   const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
     render();
   };
 
@@ -18,7 +40,9 @@
 
     for (const task of tasks) {
       htmlString += `
-        <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>${task.content}</li>
+        <li ${task.done ? 'style="text-decoration: line-through"' : ""}>${
+        task.content
+      }</li>
         `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
