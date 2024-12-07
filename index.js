f82d3a9bceb5013896efab914b251f084618ee0b
Grzegorz Oleksa <grzesieq92@gmail.com>
2024-12-07 15:19:18 +0100

    changed render function

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 {
-  const tasks = [
-    {
-      content: "zadanie 1",
-      done: false,
-    },
-    {
-      content: "zadanie 2",
-      done: true,
-    },
-  ];
+  const tasks = [];
 
   const addNewTask = (newTaskContent) => {
     tasks.push({ content: newTaskContent });
@@ -65,18 +56,19 @@
 
   const render = () => {
     let htmlString = "";
-
     for (const task of tasks) {
-      htmlString += `
-        <li ${task.done ? 'style="text-decoration: line-through"' : ""}>
-        <button class="js-done">zrobione?</button>
-        <button class="js-remove">usuń</button>
-        ${task.content}</li>
+        htmlString += `
+            <li class="list__item">
+                <button class="js-done${task.done ? " js-done--done" : ""}">${task.done ? "✔️" : ""}</button>
+                <span class="${task.done ? " list__item--done" : ""}">${task.content}</span>
+                <button class="js-remove">🗑️</button>
+            </li>
         `;
     }
+
     document.querySelector(".js-tasks").innerHTML = htmlString;
     bindEvents();
-  };
+};
 
   init();
 }
